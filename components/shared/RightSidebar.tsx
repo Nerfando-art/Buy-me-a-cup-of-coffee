544fcb8b104119c964775c9d996f3ca833f73645
Fernando <fernando@Fernandos-MBP.lan>
2023-10-03 11:35:03 -0500

    added functionality to site

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,19 @@
+function RightSidebar(){
+    return (
+        <section className="custom-scrollbar rightsidebar">
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="tex-heading4-mediume text-light-1">
+                    suggested communiies
+                </h3>
+            </div>
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="tex-heading4-mediume text-light-1">
+                    suggested users
+                </h3>
+            </div>
+
+        </section>
+    )
+}
+
+export default RightSidebar;
